4bc5a6a405b9f3f9dd99b6ac4655eb6ef562a4b5
刘庆林 <1459307172@qq.com>
2024-07-19 16:32:00 +0800

    fix: test

--- src/__tests__/main.spec.ts
+++ src/__tests__/main.spec.ts
@@ -1,20 +1,30 @@
 import { configuraInterface } from "../index";
-function sum(a: number, b: number): number {
-  return a + b;
-}
-describe("sum", () => {
-  it("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
+
 describe("configuraInterface", () => {
   it("should be defined", () => {
-    const c = new configuraInterface({
-      data: [{ id: "e", text: "33" }],
-      perm: ["e"],
+    new configuraInterface({
+      data: [{ id: "download", text: "下载" }],
+      perm: ["download"],
       fileType: "ofd",
     });
-    console.log(c);
-    expect(configuraInterface.getButtons).toEqual([{ id: "e", name: "e", text: "33" }]);
+    expect(configuraInterface.getButtons).toEqual([
+      { id: "download", name: "download", text: "下载" },
+    ]);
+    configuraInterface.hide("download");
+    expect(configuraInterface.getButtons).toEqual([]);
+    configuraInterface.show("download");
+    expect(configuraInterface.getButtons).toEqual([
+      { id: "download", name: "download", text: "下载", hide: false },
+    ]);
+    configuraInterface.disabled("download");
+    expect(configuraInterface.getButtons).toEqual([
+      {
+        id: "download",
+        name: "download",
+        text: "下载",
+        disabled: true,
+        hide: false,
+      },
+    ]);
   });
 });
